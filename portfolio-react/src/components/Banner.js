747110dc1c7cda2ef06dae5a9eb35bc820eecb05
Yoel Enrique Gutierrez Terneus <79853681+yoesk8@users.noreply.github.com>
2023-11-06 21:24:17 +0000

    Added image to banner

--- portfolio-react/src/components/Banner.js
+++ portfolio-react/src/components/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Row, Col} from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons"
+import headerImg from "../assets/img/header-img.svg"
 
 const Banner = () => {
   return (
@@ -13,6 +14,9 @@ const Banner = () => {
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
                 </Col>
+                <Col xs={12} md={6} xl={5}>
+                  <img src={headerImg} alt="Header Img"/>
+          </Col>
             </Row>
         </Container>
 
